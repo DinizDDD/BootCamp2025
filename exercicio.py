78fb0ffd2add8129a613b75d6d40989921a17d3e
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-08-26 16:14:56 -0300

    create react file

--- exercicio.py
+++
@@ -1,8 +0,0 @@
-# nome, idade, comida_favorita
-# O meu nome é ... , tenho ... anos e minha comida favorita é
-
-name = "Davi"
-age = 17
-food = "Japones"
-
-print(f"Meu nome é {name}, tenho {age} anos e minha comida favorita é {food}")
--- input.py
+++
@@ -1,7 +0,0 @@
-name = input("Qual o seu nome?")
-
-age = input("Qual a sua idade?")
-
-print(f"Seu nome é {name} e sua idade é {age}")
-
-
