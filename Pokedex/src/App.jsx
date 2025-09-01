0fef9f1f3a5170c8e9865a896f13b78394e1b332
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-09-01 16:44:57 -0300

    pokedex muito foda

---
+++ Pokedex/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import PokemonCard from './components/pokemoncard';
+import './App.css'
+
+function App() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=9");
+      const data = await response.json();
+
+      const detailedPokemon = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const pokemonResponse = await fetch(pokemon.url);
+          return await pokemonResponse.json();
+        })
+      );
+
+      setPokemons(detailedPokemon);
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <>
+      <div className="app-container">
+        <h1>Pokemodex</h1>
+        <div className="pokemon-grid">
+          {pokemons.map(pokemon => (
+            <PokemonCard
+            key={pokemon.id}
+            pokemon={pokemon}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
